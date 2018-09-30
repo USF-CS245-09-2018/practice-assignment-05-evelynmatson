d324170dca9b2d1bf2d8860f3e3920c374fd9eb8
Evelyn Matson <evelyn@Evelyns-MacBook-Air.local>
2018-09-29 21:13:57 -0500

    Tried quicksort twice, still not quite there. will need to try again.

--- QuickSort.java
+++ QuickSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class QuickSort implements SortingAlgorithm {
 
-	public void sort(int[] a, int start, int end) {
+	public void Oldsort(int[] a, int start, int end) {
 
 		System.out.println("Sorting: " + Arrays.toString(a));
 
@@ -37,17 +37,78 @@ public class QuickSort implements SortingAlgorithm {
 
 	}
 
+	public void sort(int[] a, int start, int end) {
+		System.out.println("\n\n\nStarting to sort: " + Arrays.toString(a) + " from " + start + " to " + end + ".");
+		int originalStart = start;
+		int originalEnd = end;
+
+		end--;		// end passed to function is exclusive, to avoid index out of bounds, end in the function must be a point in the array.
+
+		// Base case: trying to sort 1 item or smaller
+		if (end-start < 2) {
+			return;
+		}
+
+		// Iterative step: partition and recurse!
+		int pivot = start;
+		start++;
+
+		while (start < end) {			// Main loop. Do the work here!
+			// System.out.println("\tMain loop start. a[start] = " + a[start] + " and a[end] = " + a[end] + ".");
+			if (a[start] > a[pivot] && a[end] < a[pivot]) {		// If they should be swapped
+				System.out.println("SWAPPING: start = " + start + ", end = " + end + ", pivot = " + pivot);
+				swap(a,start,end);
+
+				// EXPERIMENTAL: increment start and decrement end?
+				// start++;
+				end--;
+			} else if (a[start] > a[pivot] && a[end] > a[pivot]) {		// If theyre both greater than a[pivot]
+				end--;	// End is ok, start can't be moved
+			} else if (a[start] < a[pivot] && a[end] < a[pivot]) {		// If thyere both lower than a[pivot]
+				start++;	// Start is ok, end can't be moved
+			} else {		// Theyre both good, so both can move on!
+				end--;
+				// start++;
+			}
+			// System.out.println("\t  Main while loop finished. start = " + start + " and end = " + end + ".");
+		}
+
+		// At this point, start and end point to the same thing AKA (start == end)
+
+		if (a[start] < a[pivot]) {		// If pivot should swap with the start/end item
+			System.out.println("SWAPPING: start = " + start + ", end = " + end + ", pivot = " + pivot);
+			swap(a,start,pivot);
+		} else {						// If pivot should swap with the item before that.
+			System.out.println("SWAPPING: start-1 = " + (start-1) + ", end = " + end + ", pivot = " + pivot);
+			swap(a,start-1,pivot);
+		}
+
+		// System.out.println("\nDone with first iteration! now:");
+		// System.out.println("\toriginalStart = " + originalStart);
+		// System.out.println("\tstart = " + start);
+		// System.out.println("\tpivot = " + pivot);
+		// System.out.println("\tend = " + end);
+		// System.out.println("\toriginalEnd = " + originalEnd);
+
+		System.out.println("Done with this recursion step. a looks like: " + Arrays.toString(a));
+
+		// Recurse now!
+		sort(a,originalStart,start);		// sort the left half
+		sort(a,start+1,originalEnd);			// sort the right half
+
+	}
+
 	public void sort(int[] a) {
 	
 		sort(a,0,a.length);
 	}
 
 	public void swap(int[] a, int swap1, int swap2) {
-		System.out.println("\t\t\tSwapping " + swap1 + " and " + swap2);
+		System.out.println("\t\tSwapping a[" + swap1 + "] = " + a[swap1] +" and a[" + swap2 + "] = " + a[swap2] + "]");
 		int temp = a[swap1];
 		a[swap1] = a[swap2];
 		a[swap2] = temp;
-		System.out.println("\t\t\tNow a looks like: " + Arrays.toString(a));
+		System.out.println("\t\tNow a looks like: " + Arrays.toString(a));
 	}
 
 	public static void main(String[] args) {
